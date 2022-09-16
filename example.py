eb2a9c30ae89228857718addd1224416a32d7560
Alessio Russo <alessior@kth.se>
2022-09-16 15:46:32 +0200

    updates"

--- example.py
+++ example.py
@@ -3,6 +3,8 @@ import scipy.signal as scipysig
 from typing import Tuple
 import scipy.stats as scipystats
 import matplotlib.pyplot as plt
+
+np.set_printoptions(formatter={'float_kind':"{:.2f}".format})
 A = np.array([[0.7, 0], [0.1, 0.3]])
 B = np.array([[0.5], [0.1]])
 
@@ -25,14 +27,14 @@ def collect_data(steps: int, std_u: float, std_w: float) -> Tuple[np.ndarray, np
 
 std_w = 0#1e-1
 std_u = 1
-sample_size = 10000
+sample_size = 1000
 
 
 X, U, W = collect_data(sample_size, std_u, std_w)
 
 attack_amplitude = 0.1
-attack_X = 0# np.random.uniform(low=0*-attack_amplitude, high=attack_amplitude, size=(dim_x, sample_size+1))
-attack_U = 1#np.random.uniform(low=0*-attack_amplitude, high=attack_amplitude, size=(dim_u, sample_size))
+attack_X = 0* np.random.uniform(low=0*-attack_amplitude, high=attack_amplitude, size=(dim_x, sample_size+1))
+attack_U = np.random.uniform(low=-attack_amplitude+10, high=attack_amplitude, size=(dim_u, sample_size))
 
 Xtilde = X + attack_X
 Utilde = U + attack_U
@@ -42,9 +44,26 @@ Dtilde = np.vstack([Xtilde[:,:-1], Utilde])
 
 AB = X[:,1:] @ np.linalg.pinv(D)
 ABtilde = Xtilde[:,1:] @ np.linalg.pinv(Dtilde)
+Delta = -B @ attack_U @ np.linalg.pinv(Dtilde)
+
+DeltaFull = np.hstack([np.eye(dim_x), -A, -B]) @ np.vstack([attack_X[:, 1:], attack_X[:, :-1], attack_U]) @ np.linalg.pinv(Dtilde)
+
+print(AB+Delta-ABtilde)
+print(AB+DeltaFull-ABtilde)
+
 
 cond_orig = np.hstack([-np.eye(dim_x), A,B]) @ np.vstack([X[:, 1:], X[:, :-1], U])
-cond_pois = np.hstack([-np.eye(dim_x), ABtilde]) @ np.vstack([Xtilde[:, 1:], Xtilde[:, :-1], Utilde])
+cond_pois = np.hstack([-np.eye(dim_x), A+Delta[:,:dim_x], B +  Delta[:, dim_x:]]) @ np.vstack([Xtilde[:, 1:], Xtilde[:, :-1], Utilde])
+cond_pois2 = np.hstack([-np.eye(dim_x), AB+Delta]) @ np.vstack([Xtilde[:, 1:], Xtilde[:, :-1], Utilde])
+
+dx=np.hstack([np.eye(dim_x), -(AB+Delta)]) @ np.vstack([Xtilde[:, 10][:,None], Xtilde[:, 9][:,None], Utilde[:,9][:,None]])
+
+V = np.hstack([np.eye(dim_x), -AB-Delta]) @ np.vstack([attack_X[:, 1:], attack_X[:, :-1], attack_U])
+V -= Delta @ D
+print(cond_orig)
+print(cond_pois)
+print(cond_pois2)
+print(V)
 import pdb
 pdb.set_trace()
 # print(AB-ABtilde)
