a5dd6975f368ac2c5815fa2be1a77d5d3c4c66b2
Alessio Russo <alessior@kth.se>
2022-10-10 15:20:19 +0200

    add residuals variance example

---
+++ example_residuals_variance/example.py
@@ -0,0 +1,108 @@
+import numpy as np
+import scipy.signal as scipysig
+from typing import Tuple, NamedTuple
+import matplotlib.pyplot as plt
+import scipy.integrate as integrate
+import cvxpy as cp
+import scipy.stats as stats
+import multiprocessing as mp
+import dccp
+from scipy.stats import chi2, norm
+from momentchi2 import hbe
+from utils import collect_data, residuals_variance_test, TestStatistics, confidence_interval_signal, ResultsData
+
+
+np.set_printoptions(formatter={'float_kind':"{:.4f}".format})
+
+dt = 0.05
+num = [0.28261, 0.50666]
+den = [1, -1.41833, 1.58939, -1.31608, 0.88642]
+sys = scipysig.TransferFunction(num, den, dt=dt).to_ss()
+trueAB = np.hstack((sys.A, sys.B))
+dim_x, dim_u = sys.B.shape
+T = 200
+NUM_SIMS = 100
+NUM_CPUS = 12
+
+std_w = 1e-1
+std_u = 1
+
+
+deltas = np.geomspace(1e-4, 1e-1, 50)
+
+true_delta_ab_norm = np.zeros((NUM_SIMS))
+true_residuals_variance = np.zeros((NUM_SIMS))
+
+poisoned_delta_ab_norm_optimal_atk = np.zeros((NUM_SIMS,len(deltas)))
+poisoned_delta_ab_norm_gaussian_atk = np.zeros((NUM_SIMS,len(deltas)))
+poisoned_residuals_variance_optimal_atk = np.zeros((NUM_SIMS,len(deltas)))
+poisoned_residuals_variance_gaussian_atk = np.zeros((NUM_SIMS,len(deltas)))
+
+tests = []
+
+
+
+
+def evaluate_attack(Xm: np.ndarray, Xp: np.ndarray, U: np.ndarray, DeltaX: np.ndarray, DeltaU: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
+    tildeXm = Xm + DeltaX[:, :-1]
+    tildeXp = Xp + DeltaX[:, 1:]
+    tildeU = U + DeltaU
+    Dtilde = np.vstack((tildeXm, tildeU))
+
+    ABtilde = tildeXp @ np.linalg.pinv(Dtilde)
+
+    residuals = tildeXp - ABtilde @ Dtilde        
+
+    return ABtilde, residuals
+
+def run_simulation(id_sim: int):
+    np.random.seed(id_sim)
+    res = ResultsData(len(deltas))
+    X, U, W = collect_data(T, std_u, std_w, sys)
+    Xp, Xm = X[:, 1:], X[:, :-1]
+    D = np.vstack((Xm, U))
+    AB = Xp @ np.linalg.pinv(D)
+
+    true_residuals = Xp - AB @ D
+    res.true_residuals_variance = residuals_variance_test(true_residuals, dim_u, [std_w ** 2] * dim_x).p_right
+    res.true_delta_ab_norm = np.linalg.norm(AB - trueAB, 2)
+
+
+    for idx_delta, delta in enumerate(deltas):
+        print(f'Running {id_sim}/{idx_delta}')
+        # Optimal attack
+        DeltaX = cp.Variable((dim_x, T+1))
+        DeltaU = cp.Variable((dim_u, T))
+
+        objective = cp.norm(DeltaX[:, 1:] - AB @ cp.vstack((DeltaX[:, :-1], DeltaU)), 'fro')
+        objective += 2*cp.trace(true_residuals @ (DeltaX[:, 1:] - AB @ cp.vstack((DeltaX[:, :-1], DeltaU))).T)
+        constraints = [
+            cp.norm(DeltaX, 'fro') <= delta * np.linalg.norm(X, 'fro'),
+            cp.norm(DeltaU, 'fro') <= delta * np.linalg.norm(U, 'fro') 
+        ]
+        problem = cp.Problem(cp.Maximize(objective), constraints)
+        result = problem.solve(method='dccp', ccp_times=10, solver=cp.MOSEK)
+
+        ABtilde, poisoned_residuals = evaluate_attack(Xm, Xp, U, DeltaX.value, DeltaU.value)
+        res.poisoned_delta_ab_norm_optimal_atk[idx_delta] = np.linalg.norm(ABtilde - trueAB, 2)
+        res.poisoned_residuals_variance_optimal_atk[idx_delta] = residuals_variance_test(poisoned_residuals, dim_u, [std_w ** 2] * dim_x).p_right
+
+        # Gaussian attack
+
+        stdX = (delta * np.linalg.norm(X, 'fro')) / np.sqrt(T* dim_x)
+        stdU = (delta * np.linalg.norm(U, 'fro')) / np.sqrt(T* dim_u)
+        
+        DeltaX = np.random.normal(size=X.shape) * stdX
+        DeltaU = np.random.normal(size=U.shape) * stdU
+
+        ABtilde, poisoned_residuals = evaluate_attack(Xm, Xp, U, DeltaX, DeltaU)
+        res.poisoned_delta_ab_norm_gaussian_atk[idx_delta] = np.linalg.norm(ABtilde - trueAB, 2)
+        res.poisoned_residuals_variance_gaussian_atk[idx_delta] = residuals_variance_test(poisoned_residuals, dim_u, [std_w ** 2] * dim_x).p_right
+
+    return res
+
+if __name__ == '__main__':
+    with mp.Pool(NUM_CPUS) as p:
+        results = p.map(run_simulation, [x for x in range(NUM_SIMS)])
+
+    np.save('data_residuals_variance.npy', results, allow_pickle=True)
---
+++ example_residuals_variance/plot.py
@@ -0,0 +1,80 @@
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import matplotlib.mathtext as mathtext
+import numpy as np
+
+from typing import List
+from utils import ResultsData, confidence_interval_signal
+
+TITLE_SIZE = 20
+LEGEND_SIZE = 18
+TICK_SIZE = 16
+AXIS_TITLE = TITLE_SIZE
+AXIS_LABEL = TITLE_SIZE
+FONT_SIZE = TITLE_SIZE
+
+mpl.rcParams['text.usetex'] = True
+mpl.rcParams['text.latex.preamble'] = r'\usepackage{{amsmath}}'
+mpl.rcParams['font.family'] = "serif"
+
+plt.rc('font', size=FONT_SIZE)          # controls default text sizes
+plt.rc('axes', titlesize=AXIS_TITLE)     # fontsize of the axes title
+plt.rc('axes', labelsize=AXIS_LABEL)    # fontsize of the x and y labels
+plt.rc('xtick', labelsize=TICK_SIZE)    # fontsize of the tick labels
+plt.rc('ytick', labelsize=TICK_SIZE)    # fontsize of the tick labels
+plt.rc('legend', fontsize=LEGEND_SIZE)    # legend fontsize
+plt.rc('figure', titlesize=TITLE_SIZE)  # fontsize of the figure title
+
+deltas = np.geomspace(1e-4, 1e-1, 50)
+NUM_SIMS = 100
+true_delta_ab_norm = np.zeros((NUM_SIMS))
+true_residuals_variance = np.zeros((NUM_SIMS))
+
+poisoned_delta_ab_norm_optimal_atk = np.zeros((NUM_SIMS,len(deltas)))
+poisoned_delta_ab_norm_gaussian_atk = np.zeros((NUM_SIMS,len(deltas)))
+poisoned_residuals_variance_optimal_atk = np.zeros((NUM_SIMS,len(deltas)))
+poisoned_residuals_variance_gaussian_atk = np.zeros((NUM_SIMS,len(deltas)))
+
+
+results: List[ResultsData] = np.load('data_residuals_variance.npy', allow_pickle=True)
+
+
+for i in range(len(results)):
+    true_delta_ab_norm[i] = results[i].true_delta_ab_norm
+    true_residuals_variance[i] = results[i].true_residuals_variance
+
+    poisoned_delta_ab_norm_optimal_atk[i,:] = results[i].poisoned_delta_ab_norm_optimal_atk
+    poisoned_delta_ab_norm_gaussian_atk[i,:] = results[i].poisoned_delta_ab_norm_gaussian_atk
+    poisoned_residuals_variance_optimal_atk[i,:] = results[i].poisoned_residuals_variance_optimal_atk
+    poisoned_residuals_variance_gaussian_atk[i,:] = results[i].poisoned_residuals_variance_gaussian_atk
+
+
+
+
+fig, ax = plt.subplots(1, 2, figsize=(12,5))
+
+impact_optimal_mean, impact_optimal_lower, impact_optimal_upper = confidence_interval_signal(poisoned_delta_ab_norm_optimal_atk, NUM_SIMS)
+impact_gaussian_mean, impact_gaussian_lower, impact_gaussian_upper = confidence_interval_signal(poisoned_delta_ab_norm_gaussian_atk, NUM_SIMS)
+ax[0].plot(deltas, impact_optimal_mean, label='Optimized attack')
+ax[0].fill_between(deltas, impact_optimal_lower, impact_optimal_upper, alpha=0.3)
+ax[0].plot(deltas, impact_gaussian_mean, label='Gaussian attack')
+ax[0].fill_between(deltas, impact_gaussian_lower, impact_gaussian_upper, alpha=0.3)
+ax[0].grid()
+ax[0].set_xscale('log')
+ax[0].set_xlabel(r'$\delta$')
+ax[0].set_ylabel(r"$\|\begin{bmatrix} \Delta A & \Delta B \end{bmatrix}\|_2$")
+
+
+resvar_optimal_mean, resvar_optimal_lower, resvar_optimal_upper = confidence_interval_signal(poisoned_residuals_variance_optimal_atk, NUM_SIMS)
+resvar_gaussian_mean, resvar_gaussian_lower, resvar_gaussian_upper = confidence_interval_signal(poisoned_residuals_variance_gaussian_atk, NUM_SIMS)
+ax[1].plot(deltas, resvar_optimal_mean, label='Optimized attack')
+ax[1].fill_between(deltas, resvar_optimal_lower, resvar_optimal_upper, alpha=0.3)
+ax[1].plot(deltas, resvar_gaussian_mean, label='Gaussian attack')
+ax[1].fill_between(deltas, resvar_gaussian_lower, resvar_gaussian_upper, alpha=0.3)
+ax[1].grid()
+ax[1].set_xlabel(r'$\delta$')
+ax[1].set_ylabel(r'$P_0(X\leq  \|\tilde R\|_{\textnormal{F}}^2 )$')
+ax[1].set_xscale('log')
+ax[0].legend()
+
+plt.savefig('example_residuals_variance.pdf',bbox_inches='tight')
