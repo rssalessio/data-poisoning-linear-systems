9158417e45ecb86cde89556b4cb0d296b37e0a62
Alessio Russo <alessior@kth.se>
2022-11-29 17:47:22 +0100

    update plots

--- example_residuals_maximization/plot_data.py
+++ example_residuals_maximization/plot_data.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 import numpy as np
+import matplotlib.ticker as mtick
 from typing import List, Tuple
 from utils import ResultsData, residuals_variance_test, confidence_interval_signal
 TITLE_SIZE = 20
@@ -101,44 +102,97 @@ impact_optimal_mean, impact_optimal_lower, impact_optimal_upper = confidence_int
 impact_gaussian_mean, impact_gaussian_lower, impact_gaussian_upper = confidence_interval_signal(gauss_poisoned_delta_norm, N_SIMS)
 impact_cem_mean, impact_cem_lower, impact_cem_upper = confidence_interval_signal(cem_poisoned_delta_norm, N_SIMS)
 
-ax[0].plot(deltas, impact_optimal_mean, label='Optimized attack')
+ax[0].plot(deltas, impact_optimal_mean, label='CCP')
 ax[0].fill_between(deltas, impact_optimal_lower, impact_optimal_upper, alpha=0.3)
-ax[0].plot(deltas, impact_cem_mean, ':', label='CEM attack')
+ax[0].plot(deltas, impact_cem_mean, ':', label='CEM')
 ax[0].fill_between(deltas, impact_cem_lower, impact_cem_upper, alpha=0.3)
-ax[0].plot(deltas, impact_gaussian_mean, '--', label='Gaussian attack')
+ax[0].plot(deltas, impact_gaussian_mean, '--', label='Random sampling')
 ax[0].fill_between(deltas, impact_gaussian_lower, impact_gaussian_upper, alpha=0.3)
 
 ax[0].grid()
 ax[0].set_xscale('log')
 ax[0].set_xlabel(r'$\delta$')
-ax[0].set_ylabel(r"$\mathrm{E}\|\begin{bmatrix} \Delta A & \Delta B \end{bmatrix}\|_2$")
+ax[0].set_ylabel(r"$\mathrm{E}\|\begin{bmatrix} \Delta \tilde A_{\textrm{LS}} & \Delta \tilde B_{\textrm{LS}}\end{bmatrix}\|_2$")
 
-resvar_optimal_mean, resvar_optimal_lower, resvar_optimal_upper = confidence_interval_signal(opt_poisoned_delta_test, N_SIMS)
-resvar_gaussian_mean, resvar_gaussian_lower, resvar_gaussian_upper = confidence_interval_signal(gauss_poisoned_delta_test, N_SIMS)
-resvar_cem_mean, resvar_cem_lower, resvar_cem_upper = confidence_interval_signal(cem_poisoned_delta_test, N_SIMS)
+# inset axes
+axins = ax[0].inset_axes([0.2, 0.5, 0.45, 0.4], transform=ax[0].transAxes)
+axins.plot(deltas, impact_optimal_mean, label='CCP')
+axins.fill_between(deltas, impact_optimal_lower, impact_optimal_upper, alpha=0.3)
+axins.plot(deltas, impact_cem_mean, ':', label='CEM')
+axins.fill_between(deltas, impact_cem_lower, impact_cem_upper, alpha=0.3)
+axins.plot(deltas, impact_gaussian_mean, '--', label='Random sampling')
+axins.fill_between(deltas, impact_gaussian_lower, impact_gaussian_upper, alpha=0.3)
 
-ax[1].plot(deltas, resvar_optimal_mean, label='Optimized attack')
+# sub region of the original image
+axins.set_xlim(1e-3, 1e-2)
+axins.set_ylim(0, 0.05)
+axins.grid()
+axins.set_xticklabels([])
+# yticks = axins.get_yticks()
+# axins.set_yticklabels(["{:.1e}".format(x) for x in yticks])
+
+# axins.ticklabel_format(axis='y', style='sci')
+
+ax[0].indicate_inset_zoom(axins, edgecolor="black", linewidth=1.5)
+
+
+
+
+
+
+
+
+
+resvar_optimal_mean, resvar_optimal_lower, resvar_optimal_upper = confidence_interval_signal(1-opt_poisoned_delta_test, N_SIMS)
+resvar_gaussian_mean, resvar_gaussian_lower, resvar_gaussian_upper = confidence_interval_signal(1-gauss_poisoned_delta_test, N_SIMS)
+resvar_cem_mean, resvar_cem_lower, resvar_cem_upper = confidence_interval_signal(1-cem_poisoned_delta_test, N_SIMS)
+
+ax[1].plot(deltas, resvar_optimal_mean, label='CCP')
 ax[1].fill_between(deltas, resvar_optimal_lower, resvar_optimal_upper, alpha=0.3)
-ax[1].plot(deltas, resvar_cem_mean, ':', label='CEM attack')
+ax[1].plot(deltas, resvar_cem_mean, ':', label='CEM')
 ax[1].fill_between(deltas, resvar_cem_lower, resvar_cem_upper, alpha=0.3)
-ax[1].plot(deltas, resvar_gaussian_mean, '--', label='Gaussian attack')
+ax[1].plot(deltas, resvar_gaussian_mean, '--', label='Random sampling')
 ax[1].fill_between(deltas, resvar_gaussian_lower, resvar_gaussian_upper, alpha=0.3)
 
 ax[1].grid()
 ax[1].set_xlabel(r'$\delta$')
-ax[1].set_ylabel(r'$P_0(X\leq  \|\tilde R\|_{\textnormal{F}}^2 )$')
+ax[1].set_ylabel(r'$p$-value')
 ax[1].set_xscale('log')
+
+# inset axes
+axins = ax[1].inset_axes([0.6, 0.54, 0.35, 0.35], transform=ax[1].transAxes)
+
+axins.plot(deltas, resvar_optimal_mean, label='CCP')
+axins.fill_between(deltas, resvar_optimal_lower, resvar_optimal_upper, alpha=0.3)
+axins.plot(deltas, resvar_cem_mean, ':', label='CEM')
+axins.fill_between(deltas, resvar_cem_lower, resvar_cem_upper, alpha=0.3)
+axins.plot(deltas, resvar_gaussian_mean, '--', label='Random sampling')
+axins.fill_between(deltas, resvar_gaussian_lower, resvar_gaussian_upper, alpha=0.3)
+
+# sub region of the original image
+axins.set_xlim(1e-3, 1e-2)
+axins.set_ylim(0, 1e-3)
+axins.set_yscale('symlog')
+axins.grid()
+axins.set_xticklabels([])
+# yticks = axins.get_yticks()
+# axins.set_yticklabels(["{:.1e}".format(x) for x in yticks])
+
+# axins.ticklabel_format(axis='y', style='sci')
+
+ax[1].indicate_inset_zoom(axins, edgecolor="black",linewidth=2)
+
 
 
 dot_optimal_mean, dot_optimal_lower, dot_optimal_upper = confidence_interval_signal(opt_poisoned_delta_dot, N_SIMS)
 dot_gaussian_mean, dot_gaussian_lower, dot_gaussian_upper = confidence_interval_signal(gauss_poisoned_delta_dot, N_SIMS)
 dot_cem_mean, dot_cem_lower, dot_cem_upper = confidence_interval_signal(cem_poisoned_delta_dot, N_SIMS)
 
-ax[2].plot(deltas, dot_optimal_mean, label='Optimized attack')
+ax[2].plot(deltas, dot_optimal_mean, label='CCP')
 ax[2].fill_between(deltas, dot_optimal_lower, dot_optimal_upper, alpha=0.3)
-ax[2].plot(deltas, dot_cem_mean, ':', label='CEM attack')
+ax[2].plot(deltas, dot_cem_mean, ':', label='CEM')
 ax[2].fill_between(deltas, dot_cem_lower, dot_cem_upper, alpha=0.3)
-ax[2].plot(deltas, dot_gaussian_mean, '--', label='Gaussian attack')
+ax[2].plot(deltas, dot_gaussian_mean, '--', label='Random sampling')
 ax[2].fill_between(deltas, dot_gaussian_lower, dot_gaussian_upper, alpha=0.3)
 
 ax[2].grid()
@@ -150,6 +204,11 @@ print([r"$." + str(round(r/np.pi,2))[2:]+ r"\pi$" for r in ax[2].get_yticks()])
 ax[2].set_yticklabels([r"$." + str(round(r/np.pi,2))[2:]+ r"\pi$" for r in ax[2].get_yticks()])
 
 
-plt.legend(bbox_to_anchor=(0.750, 0.875), loc="lower right",
-                bbox_transform=fig.transFigure, ncol=3, frameon=False)
-plt.savefig('figures/example_residuals_variance.pdf',bbox_inches='tight',dpi=300)
+h, l = ax[0].get_legend_handles_labels() # Extracting handles and labels
+ph = [plt.plot([],marker="", ls="")[0]] # Canvas
+handles = ph + h
+labels = ["Method:"] + l  # Merging labels
+
+plt.legend(handles, labels, bbox_to_anchor=(0.750, 0.875), loc="lower right",
+                bbox_transform=fig.transFigure, ncol=4, frameon=False)
+plt.savefig('figures/example_residuals_variance.png',bbox_inches='tight',dpi=300)
